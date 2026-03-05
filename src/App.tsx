303165d71c7788dc7e30a5ecd7c3d6d3b0b24898
LamInThisWeirdWorld <lamishere1715@gmail.com>
2026-03-05 17:40:26 +1100

    feature: srink and moveaway No button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import hamsterGif from "./assets/Cry Crying GIF by Sad Hamster.gif";
 import catGif from "./assets/Cat Moving GIF.gif";
+import texts from "./noButtonText";
 
 const App = () => {
   const [sizeNo, setSizeNo] = useState(1);
   const [sizeYes, setSizeYes] = useState(1);
+  const [positionNo, setPositionNo] = useState({ x: 0, y: 0 });
+  const [textNoIndex, setTextNoIndex] = useState(0);
+
+  const maxX = window.innerWidth;
+  const maxY = window.innerHeight;
 
   const handleClickA = () => {
-    setSizeNo((prev) => Math.max(prev - 0.1, 0.3));
-    setSizeYes((prev) => prev + 0.1);
+    setSizeNo((prev) => Math.max(prev - 0.1, 0.4));
+    setSizeYes((prev) => Math.min(prev + 0.1, 1.5));
+    setTextNoIndex((prev) => (prev + 1) % texts.length);
+
+    if (sizeNo <= 0.5) {
+      setPositionNo({
+        x: Math.random() * maxX - maxX / 3,
+        y: Math.random() * maxY - maxY / 3,
+      });
+    }
   };
 
   return (
@@ -19,17 +33,19 @@ const App = () => {
       <div className="flex gap-10">
         <button
           onClick={handleClickA}
-          className="bg-green-600 w-50 h-20 text-2xl rounded-lg text-white"
+          className="bg-green-600 w-70 h-20 text-2xl rounded-lg text-white"
           style={{ transform: `scale(${sizeYes})` }}
         >
-          of course
+          of course nhớ chứ :)))
         </button>
         <button
           onClick={handleClickA}
-          className="bg-red-600 w-50 h-20 text-2xl rounded-lg text-white"
-          style={{ transform: `scale(${sizeNo})` }}
+          className="bg-red-600 w-70 h-20 text-2xl rounded-lg text-white"
+          style={{
+            transform: `scale(${sizeNo}) translate(${positionNo.x}px, ${positionNo.y}px)`,
+          }}
         >
-          nope
+          {texts[textNoIndex]}
         </button>
       </div>
     </div>
---
+++ src/noButtonText.tsx
@@ -0,0 +1,15 @@
+const texts = [
+  "Nope",
+  "Really???",
+  "Are u sure?????",
+  "Ơ kìa :(((((",
+  "Thiệt lun",
+  "im gonna cryyyyyy",
+  "Nooo ai cho bấm :((",
+  "Bye :)))))))))",
+  "Đố c bắt đc e~~",
+  "Khum",
+  "Bắt buộc phải nhớ",
+];
+
+export default texts;
