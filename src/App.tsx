e043dbce00c868e9ac3dfaee91fe157a54e0ef54
LamInThisWeirdWorld <lamishere1715@gmail.com>
2026-03-10 10:37:15 +1100

    add gifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
 import { useEffect, useState } from "react";
 import hamsterGif from "./assets/Cry Crying GIF by Sad Hamster.gif";
 import catGif from "./assets/Cat Moving GIF.gif";
-import texts from "./noButtonText";
+import { texts, gif } from "./noButtonText";
+import raccoonHeart from "./assets/Night Sleep GIF by 644.gif";
 
 const App = () => {
+  const [mainText, setMainText] = useState("Will u miss me");
   const [sizeNo, setSizeNo] = useState(1);
   const [sizeYes, setSizeYes] = useState(1);
   const [positionNo, setPositionNo] = useState({ x: 0, y: 0 });
   const [textNoIndex, setTextNoIndex] = useState(0);
   const [stop, setStop] = useState(false);
+  const [gifIndex, setGifIndex] = useState(0);
+  const [mainGif, setMainGif] = useState(gif[gifIndex]);
 
   const maxX = window.innerWidth;
   const maxY = window.innerHeight;
 
   const handleClickA = () => {
     setSizeNo((prev) => Math.max(prev - 0.1, 0.4));
-    setSizeYes((prev) => Math.min(prev + 0.1, 1.5));
     setTextNoIndex((prev) => (prev + 1) % texts.length);
+    setMainGif(gif[gifIndex + 1]);
+    setGifIndex((prev) => (prev + 1) % gif.length);
+    setSizeYes((prev) => Math.min(prev + 0.1, 1.8));
 
     if (sizeNo <= 0.5) {
       setPositionNo({
-        x: Math.random() * maxX - maxX / 3,
-        y: Math.random() * maxY - maxY / 3,
+        x: Math.random() * maxX - maxX / 5,
+        y: Math.random() * maxY - maxY / 5,
       });
     }
+  };
+
+  const handleClickB = () => {
+    setMainText("Hihi I'll miss u too :33");
+    setMainGif(raccoonHeart);
+    setStop(true);
   };
 
   useEffect(() => {
@@ -33,8 +45,9 @@ const App = () => {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
 
-        const offset = 40;
+        const offset = 35;
 
+        // cai logic nay co van de
         setPositionNo((prev) => ({
           x: mouseX < maxX / 2 ? prev.x + offset : prev.x - offset,
           y: mouseY < maxY / 2 ? prev.y + offset : prev.y - offset,
@@ -47,22 +60,24 @@ const App = () => {
   }, [textNoIndex]);
 
   return (
-    <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-5">
-      <h1 className="text-white text-6xl">Will u miss me</h1>
-      <img src={catGif} alt="cat gif" className="w-64" />
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
+      <h1 className="text-white text-6xl">{mainText}</h1>
+      <img src={mainGif} alt="cat gif" className="w-64" />
 
       <div className="flex gap-10">
         <button
-          onClick={handleClickA}
-          className="bg-green-600 w-70 h-20 text-2xl rounded-lg text-white"
-          style={{ transform: `scale(${sizeYes})` }}
+          onClick={handleClickB}
+          className="bg-green-600 w-70 h-20 text-2xl rounded-lg text-white transition-all duration-200"
+          style={{
+            transform: `scale(${sizeYes})`,
+          }}
         >
           of course nhớ chứ :)))
         </button>
         <button
           onClick={handleClickA}
           disabled={stop}
-          className="bg-red-600 w-70 h-20 text-2xl rounded-lg text-white transition-all duration-100"
+          className="bg-red-600 w-70 h-20 text-2xl rounded-lg text-white transition-all duration-75"
           style={{
             transform: `scale(${sizeNo}) translate(${positionNo.x}px, ${positionNo.y}px)`,
           }}
--- src/noButtonText.tsx
+++ src/noButtonText.tsx
@@ -1,3 +1,21 @@
+import pikachu from "./assets/Pokemon Reaction GIF.gif";
+import sadCat from "./assets/Sad Cat GIF.gif";
+import catMovingGif from "./assets/Cat Moving GIF.gif";
+import sadDog from "./assets/Sad Dog GIF.gif";
+import danceDog from "./assets/Dance Love GIF.gif";
+import cryRabbit from "./assets/Sad Cry GIF by beckycas.gif";
+import cryBear from "./assets/Sad Cry GIF by LINE FRIENDS.gif";
+import raccoonWait from "./assets/Emoji Waiting GIF by 644.gif";
+import raccoonLan from "./assets/giphy.gif";
+import raccoonCorner from "./assets/Tired Emoji GIF by 644.gif";
+import raccoonReally from "./assets/Emoji What GIF by 644.gif";
+import racoonShock from "./assets/Emoji What GIF by 644 (1).gif";
+import racoonAngry from "./assets/Emoji No GIF by 644.gif";
+import racconCry from "./assets/Sad Cry GIF by 644.gif";
+import racoonHit from "./assets/Angry Emoji GIF by 644.gif";
+import raccoonFire from "./assets/Angry Emoji GIF by 644 (1).gif";
+import racoonFall from "./assets/Emoji Falling GIF by 644.gif";
+
 const texts = [
   "Nope",
   "Really???",
@@ -11,4 +29,20 @@ const texts = [
   "Đố c bắt đc e~~",
 ];
 
-export default texts;
+const gif = [
+  //   catMovingGif,
+  raccoonWait,
+  raccoonReally,
+  raccoonCorner,
+  racoonShock,
+  raccoonLan,
+  racconCry,
+  racoonAngry,
+  racoonHit,
+  raccoonFire,
+  racoonFall,
+
+  danceDog,
+];
+
+export { texts, gif };
